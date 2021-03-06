1c91d0a21a28cf0159581fc42490a756a78eccdf
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-03-06 15:40:50 -0500

    Ice Cream Assignment

---
+++ ice-cream.js
@@ -0,0 +1,10 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]); // First element
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]); // Last element
+console.log(iceCreamFlavours.length); // Length of array
