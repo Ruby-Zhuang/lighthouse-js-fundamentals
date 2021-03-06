ae14c7c8e6ea1d08979670c9d8373f7adaa4b803
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-03-05 21:59:25 -0500

    chooseStations function assignment that takes in an array of possible voting stations, and then only returns the names of the stations that are appropriate.

---
+++ chooseStations.js
@@ -0,0 +1,22 @@
+const chooseStations = function (stations) {
+  const goodStations = [];
+
+  // Loop through all the stations to check for capacity and type
+  for (const station of stations) {
+    const capacity = station[1];
+    const type = station[2];
+
+    if ((capacity >= 20) && (type === "school" || type === "community centre")) {
+      goodStations.push(station[0]);  
+    }
+  }
+  return goodStations;
+}
+
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+console.log(chooseStations(stations));
