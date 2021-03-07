4f19d67fbd4f7b04ba765421a8cadf3fe62d237d
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-03-06 22:22:23 -0500

    extension of the previous Loopy Lighthouse Challenge to make code more reusable

--- loopy-lighthouse2.js
+++ loopy-lighthouse2.js
@@ -1,7 +1,7 @@
 function loopyLighthouse(range, multiples, words) {
   for (let num = range[0]; num <= range[1]; num++){
     if ((num % multiples[0] === 0) && (num % multiples[1] === 0)){
-      console.log(words[0]+words[1]);
+      console.log(words[0] + words[1]);
     } else if (num % multiples[0] === 0){
       console.log(words[0]);
     } else if (num % multiples[1] === 0){
